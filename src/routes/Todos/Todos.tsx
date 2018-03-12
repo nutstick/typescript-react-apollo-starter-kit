8c7e859cc009242f90631638c4aa480f4d58312c
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2018-03-12 14:02:07 +0700

    Todo page

--- src/routes/Todos/Todos.tsx
+++ src/routes/Todos/Todos.tsx
@@ -1,18 +1,85 @@
+import * as cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import * as React from 'react';
+import { AddTodoMutation } from '../../apollo/todo/AddTodoMutation';
+import { TodosQuery } from '../../apollo/todo/TodosQuery';
+import { ToggleTodoMutation } from '../../apollo/todo/ToggleTodoMutation';
 import * as s from './Todos.css';
 
 export namespace Todos {
   export type Props = any;
+
+  export interface State {
+    addTodoData: string;
+  }
 }
 
 @withStyles(s)
-export class Todos extends React.Component<Todos.Props> {
+export class Todos extends React.Component<Todos.Props, Todos.State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addTodoData: '',
+    };
+  }
+
+  private onAddTodoChange(event) {
+    this.setState({
+      addTodoData: event.target.value,
+    });
+  }
+
   public render() {
-    return <div className={s.root}>
-      <h4>Todos</h4>
-      <div>
-      </div>
-    </div>;
+    const { addTodoData } = this.state;
+    const onAddTodoChange = this.onAddTodoChange.bind(this);
+    const forceUpdate = this.forceUpdate.bind(this);
+    return (
+      <TodosQuery query={TodosQuery.query}>
+        {({ loading, error, data, refetch }) => {
+          if (loading) { return 'loading'; }
+          const todos = error || !data ? [] : data.todos;
+
+
+          return <AddTodoMutation mutation={AddTodoMutation.mutation}>
+            {(addTodo) => {
+              const onFormSubmit = (e) => {
+                e.preventDefault();
+                addTodo({ variables: { text: addTodoData }});
+                // FIXME: Any way to better update component
+                forceUpdate();
+              };
+              return <ToggleTodoMutation mutation={ToggleTodoMutation.mutation}>
+                {(toggleTodo) => {
+                  const onToggleTodo = (e, id) => {
+                    e.preventDefault();
+                    toggleTodo({ variables: { id }})
+                      .then(() => refetch());
+                  };
+                  return <div className={s.root}>
+                    <h4>Todos</h4>
+                    <form onSubmit={onFormSubmit}>
+                      <input type="text" value={addTodoData} onChange={onAddTodoChange}/>
+                    </form>
+                    <div className={s.todoList}>
+                      {todos.map(({ id, done, text }) => (
+                        <div className={s.todo} key={id}>
+                          <div
+                            className={cx(s.grapper, { [s.done]: done })}
+                            onClick={onToggleTodo.bind(this, id)}
+                          ></div>
+                          <div className={cx(s.text, { [s.done]: done })}>
+                            {text}
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>;
+                }}
+              </ToggleTodoMutation>;
+            }}
+          </AddTodoMutation>;
+        }}
+      </TodosQuery>
+    );
   }
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import { Layout } from '../components/Layout';
 import { Home } from '../routes/Home';
 import { NotFound } from '../routes/NotFound';
+import { Todos } from '../routes/Todos';
 
 export default (props) => (
   <Layout>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/examples/todo" component={Todos} />
       <Route component={NotFound} />
     </Switch>
   </Layout>
